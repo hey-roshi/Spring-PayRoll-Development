8ec2ab453aa0469d782cb691f52b4701fe6fcaaa
roshi <srivastavaroshi00@gmail.com>
2025-03-06 11:30:02 +0530

    added dto

---
+++ Spring-Employee-PayRoll-Dev/src/main/java/com/example/SpringEmployeePayRollDev/dto/EmployeeDTO.java
@@ -0,0 +1,11 @@
+package com.example.SpringEmployeePayRollDev.dto;
+
+public class EmployeeDTO {
+    public String name;
+    public double salary;
+
+    public EmployeeDTO(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
