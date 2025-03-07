95a42cbfe4274c932c3187858ffa1d19a86e1561
roshi <srivastavaroshi00@gmail.com>
2025-03-07 17:05:13 +0530

    user-friendly error responses for validation failures

---
+++ Spring-Employee-PayRoll-Dev/src/main/java/com/example/SpringEmployeePayRollDev/exception/CustomExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.example.SpringEmployeePayRollDev.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                "Validation Failed",
+                errors.toString()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ Spring-Employee-PayRoll-Dev/src/main/java/com/example/SpringEmployeePayRollDev/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.example.SpringEmployeePayRollDev.exception;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
